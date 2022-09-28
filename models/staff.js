a8467fbade359cf7cbc1304732bbf4cdc06c93c9
sinh <ben.transinh@gmail.com>
2022-09-28 20:53:38 +0700

    class object contruction

---
+++ models/staff.js
@@ -0,0 +1,16 @@
+// Cấu trúc thư mục mỗi lớp đối tượng một file
+
+function Staff(id, fullName, email, password, workDay, salary, position, workHours, totalSalary, staffType) {
+    this.staffId = id;
+    this.fullName = fullName;
+    this.email = email;
+    this.password = password;
+    this.workDay = workDay;
+    this.salary = salary;
+    this.position = position;
+    this.workingHours = workHours;
+    this.totalSalary = totalSalary;
+    this.staffType =  staffType;
+
+
+}
